8e598eeb2dc5128fd56e9c8a8118490a9d1a3682
mjperrera <mjperrera@yahoo.com>
2020-03-13 15:29:15 +0800

    initial commit

---
+++ MyQrCodeGeneratorAndScanner/app/src/main/java/com/example/myqrcodegeneratorandscanner/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.myqrcodegeneratorandscanner;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imv;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imv = findViewById(R.id.imageViewBitmap);
+        editText = findViewById(R.id.editText);
+    }
+
+    public void GenerateClick(View view) {
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode(editText.getText().toString(), BarcodeFormat.QR_CODE, 640, 640); //512
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            //image overlay
+            Bitmap logo = BitmapFactory.decodeResource(getResources(), R.drawable.ic_cebuana);
+            imv.setImageBitmap(mergeBitmaps(logo, bmp));
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Bitmap mergeBitmaps(Bitmap overlay, Bitmap bitmap) {
+        int height = bitmap.getHeight();
+        int width = bitmap.getWidth();
+        Bitmap combined = Bitmap.createBitmap(width, height, bitmap.getConfig());
+        Canvas canvas = new Canvas(combined);
+        int canvasWidth = canvas.getWidth();
+        int canvasHeight = canvas.getHeight();
+        canvas.drawBitmap(bitmap, new Matrix(), null);
+        int centreX = (canvasWidth - overlay.getWidth()) / 2;
+        int centreY = (canvasHeight - overlay.getHeight()) / 2;
+        canvas.drawBitmap(overlay, centreX, centreY, null);
+        return combined;
+    }
+
+
+}
